1ad880403a7060b24abde29cf61823bd1ef62819
kazu-taka <k.takahashi@nowall.co.jp>
2018-04-04 18:27:39 +0900

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-100.times do |index|
-School.create(title:"迷いの中に生きています",name: "山本太郎",career: 20, post.position_name: "小学校教員")
-end
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Post.create(position_name: "幼稚園教員,保育士")
@@ -12,3 +9,8 @@ Post.create(position_name: "小学校教員")
 Post.create(position_name: "中学校教員")
 Post.create(position_name: "高校教員")
 Post.create(position_name: "大学教員")
+
+post_id = Post.first
+100.times do |index|
+School.create(title:"迷いの中に生きています",name: "山本太郎",career: 20, post_id: post_id)
+end
